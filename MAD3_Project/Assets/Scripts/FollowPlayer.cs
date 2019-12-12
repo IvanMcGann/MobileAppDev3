1208134d377335ae1e648fa5b7a8f683ed5f2a85
Ivan McGann <g00340138@gmit.ie>
2019-12-12 23:15:47 +0000

    Started project over as a 2D shooter, will add more to it as I continue as the project is in a prototype status at present with no menu or score being present.

---
+++ MAD3_Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject playerObject;  // the player object to follow
+    public float lerpSpeed = 0.5f;
+    private Vector3 offset;
+
+    void Start()
+    {
+        // get the currentr offset between player and camarea positions
+        offset = transform.position - playerObject.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position,                         // current camera position
+            playerObject.transform.position + offset,   // new position plus our original offset
+            lerpSpeed);                                 // the speed of smoothing
+    }
+}
---
+++ MAD3_Project/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+        public float moveSpeed = 5f;
+        public float hitPoints = 100f;
+        private Rigidbody2D rb;
+        public GameObject bulletPrefab;     // the prefab of our bullet
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("Warning on Player: Start method cannot find RigidBody2D ");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+
+            // convert user input into world movement
+            float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+            float verticalMovement = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+            //assign movement to a single vector3
+            Vector3 directionOfMovement = new Vector3(horizontalMovement, verticalMovement, 0);
+
+            // apply movement to player's transform
+            gameObject.transform.Translate(directionOfMovement);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // if the player pressed space (exclude holding key down)
+            GameObject go = Instantiate(bulletPrefab, gameObject.transform);
+            Bullet bullet = go.GetComponent<Bullet>();
+            bullet.targetVector = new Vector3(1, 1, 0);
+        }
+
+    }
+
+    // this is called at a fixed interval for use with physics objects like the RigidBody2D
+    void FixedUpdate()
+    {
+        // check if user has pressed some input keys
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+
+            // convert user input into world movement
+            float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+            float verticalMovement = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+            //assign world movements to a Veoctor2
+            Vector2 directionOfMovement = new Vector2(horizontalMovement, verticalMovement);
+
+            // apply movement to player's transform
+            rb.AddForce(directionOfMovement);
+        }
+    }
+}
