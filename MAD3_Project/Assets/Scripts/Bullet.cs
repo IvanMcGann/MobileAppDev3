d3d55ee22d0a5bcf33660a1ae19fa6f970a9702f
Ivan McGann <g00340138@gmit.ie>
2019-12-17 17:44:17 +0000

    Tried to implement audio but my Unity environment won't play audio for some reason that I cannot solve, now I am moving on to creating the main menu.

--- MAD3_Project/Assets/Scripts/Bullet.cs
+++ MAD3_Project/Assets/Scripts/Bullet.cs
@@ -15,6 +15,7 @@ public class Bullet : MonoBehaviour
         Rigidbody2D rb = gameObject.GetComponentInChildren<Rigidbody2D>();
         // add force 
         rb.AddForce(targetVector.normalized * speed);
+        AudioManager.instance.PlaySound("Laser");//audio for shooting
     }
 
 
---
+++ MAD3_Project/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class MainMenuController : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void options()
+    {
+
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
