0bdd626fc5d9d3499b56b95f0981f8580805a601
Ivan McGann <g00340138@gmit.ie>
2019-12-17 14:26:29 +0000

    Added multiple enemies and a very basic layer sprite, if I have time I will sort out my sprites but the deadline is very tight right now.

---
+++ MAD3_Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int enemyLimit = 20;
+    private int currentEnemyCount = 0;
+
+    public GameObject enemyPrefab;
+
+    public Rect SpawnArea;
+    // Update is called once per frame
+    void Update()
+    {
+        // do we have enough enemies
+        if (currentEnemyCount < enemyLimit)
+        {
+            // no, no we do not! so create one.
+            Vector3 spawnLocation = new Vector3(
+                Random.Range(SpawnArea.x, SpawnArea.y),         // x position
+                Random.Range(SpawnArea.width, SpawnArea.height),// y position
+                0);                                             // z position 
+
+            GameObject go = Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
+            currentEnemyCount += 1; // we made one, so note it down in our var
+        }
+    }
+
+    // we got one! pew pew
+    public void enemyDied()
+    {
+        currentEnemyCount--;
+    }
+}
